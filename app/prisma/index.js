327443880789a32d6a920b261a0640157d02dd51
Nurul Sundarani <sundarani@prisma.io>
2023-11-23 16:42:00 +0530

    Chnaged import

--- app/prisma/index.js
+++ app/prisma/index.js
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client/edge.js";
+import { PrismaClient } from "@prisma/client";
 import { withAccelerate } from "@prisma/extension-accelerate";
 
 if (!globalThis.prisma) {
